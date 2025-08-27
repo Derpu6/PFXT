231d94ea25fa87f45dc85c8fc6729995a2e8bac0
Derpu6 <puj8657@gmail.com>
2025-08-27 16:54:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ import re
 import ast
 import pandas as pd
 import numpy as np
-import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import hashlib
 import difflib
@@ -1315,5 +1313,4 @@ if __name__ == "__main__":
         show_learning_feedback()
 
     elif app_mode == "抄袭情况":
-
         show_plagiarism_report()
